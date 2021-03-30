82697a462aecee13382e549186d4d8e0bc897770
Rafael Garcia <rafaelgarcia95@gmail.com>
2021-03-29 22:49:36 -0300

    Handle points coordinates from txt file

--- 1504940/closest.c
+++ 1504940/closest.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
 
-int main() {
+int main(int argc, char **argv) {
     
-    printf("Hello world");
+    FILE *input_file;
+
+    int point_quantity;
+   	int counter;
+    double coordinate_x;
+    double coordinate_y;
+
+    input_file = fopen(argv[1], "r");
+
+    // Getting the quantity of points present in the input.txt file
+    fscanf(input_file, "%d", &point_quantity);
+
+    // Printing all points coordinates from input.txt file
+    counter = 0;
+    while(counter != point_quantity) {
+
+    	fscanf(input_file, "%lf", &coordinate_x);
+    	fscanf(input_file, "%lf", &coordinate_y);
+
+    	printf("%.15f ", coordinate_x);
+    	printf("%.15f \n", coordinate_y);
+
+    	counter++;
+    }
+
+    // Closing the file pointer
+    fclose(input_file);
 
     return 0;
 }
