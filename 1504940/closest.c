afe92b468e0fc7bf7c59af74b2753bfd2553761e
Rafael Garcia <rafaelgarcia95@gmail.com>
2021-03-30 20:33:20 -0300

    Compute time taken to perform brute force and print relevant results

--- 1504940/closest.c
+++ 1504940/closest.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
 
 float calculate_distance(float x, float y, float z, float w) {
-   return sqrt(pow(z - x,2) + pow(w-y,2));
+   return sqrt(pow(z - x, 2) + pow(w - y, 2));
 }
 
 int main(int argc, char **argv) {
     
     FILE *input_file;
+
+    clock_t t;
+    double time_taken;
     
     int coordinates_start;
-    int next_check;
+    int coordinates_next_check;
 
     int point_quantity;
     int counter_i;
@@ -24,6 +28,10 @@ int main(int argc, char **argv) {
     double coordinate_w;
 
     double min_distance = 999999999999999;
+    double final_coordinate_x;
+    double final_coordinate_y;
+    double final_coordinate_z;
+    double final_coordinate_w;
 
     input_file = fopen(argv[1], "r");
 
@@ -33,20 +41,23 @@ int main(int argc, char **argv) {
     // Storing the file pointer position of the coordinates start
     coordinates_start = ftell(input_file);
 
-    for (counter_i = 0; counter_i <= point_quantity; counter_i++) {
+    // Starting the clock count
+    t = clock();
+
+    for (counter_i = 0; counter_i < point_quantity; counter_i++) {
         fscanf(input_file, "%lf", &coordinate_x);
         fscanf(input_file, "%lf", &coordinate_y);
 
         // Storing the file pointer position for the next set of coordinates to compare
-        next_check = ftell(input_file);
+        coordinates_next_check = ftell(input_file);
 
-        printf("%.15f ", coordinate_x);
+        printf("\n%.15f ", coordinate_x);
         printf("%.15f \n", coordinate_y);
 
         // Rewinding the file pointer to the start
         fseek(input_file, coordinates_start, SEEK_SET);
 
-        for (counter_j = 0; counter_j <= point_quantity; counter_j++) {
+        for (counter_j = 0; counter_j < point_quantity; counter_j++) {
             fscanf(input_file, "%lf", &coordinate_z);
             fscanf(input_file, "%lf", &coordinate_w);
 
@@ -54,6 +65,10 @@ int main(int argc, char **argv) {
 
             if ((this_distance != 0) && (this_distance < min_distance)) {
                 min_distance = this_distance;
+                final_coordinate_x = coordinate_x;
+                final_coordinate_y = coordinate_y;
+                final_coordinate_z = coordinate_z;
+                final_coordinate_w = coordinate_w;
             }
 
             printf("\t %.15f ", coordinate_z);
@@ -62,11 +77,26 @@ int main(int argc, char **argv) {
         }
 
         // Setting the pointer to the next set of coordinates that we want to compare
-        fseek(input_file, next_check, SEEK_SET);
+        fseek(input_file, coordinates_next_check, SEEK_SET);
     }
+
+    // Getting the clock quantity that the algorithm took to run
+    t = clock() -t;
 
     // Closing the file pointer
     fclose(input_file);
 
+    // Printing the results
+    time_taken = ((double) t) / CLOCKS_PER_SEC;
+
+    printf("\n Brute force took %.4f seconds\n", time_taken);
+    printf("\n Brute force found %.4f to be the min distance between two points\n", min_distance);
+    printf("\n Brute force found %.4f to be the first point x coordinate\n", final_coordinate_x);
+    printf("\n Brute force found %.4f to be the first point y coordinate\n", final_coordinate_y);
+    printf("\n Brute force found %.4f to be the second point x coordinate\n", final_coordinate_z);
+    printf("\n Brute force found %.4f to be the second point y coordinate\n", final_coordinate_w);
+
     return 0;
 }
+
+
